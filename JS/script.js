0089556caf474ad5c2caf6695e8291ceee7cf16c
Daniele206 <danieledeieso2004@gmail.com>
2024-02-20 15:09:03 +0100

    [add] calc full-tiket-cost

---
+++ JS/script.js
@@ -0,0 +1,10 @@
+const chilometers = prompt ('Quanti chilometri devi percorrere?');
+console.log(chilometers);
+
+const age = prompt ('Quanti anni hai?');
+console.log(age);
+
+const costPerKm = 0.21;
+
+const fullCostTiket = chilometers * costPerKm;
+console.log(fullCostTiket);
