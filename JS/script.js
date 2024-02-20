b91878fcdcb352158f3e1de7d49259adb570531d
Daniele206 <danieledeieso2004@gmail.com>
2024-02-20 15:31:47 +0100

    [add] tiket cost too page

--- JS/script.js
+++ JS/script.js
@@ -15,10 +15,14 @@ console.log(discountTiketUnder18);
 const discountTiketOver65 = fullCostTiket * 40 / 100;
 console.log(discountTiketOver65);
 
-let finalCostTiket = '';
+let finalCostTiket = fullCostTiket;
 
 if(age < 18) finalCostTiket = fullCostTiket - discountTiketUnder18;
 
 if(age > 65) finalCostTiket = fullCostTiket - discountTiketOver65;
 
 console.log(finalCostTiket);
+
+document.getElementById('biglietto').innerHTML = `
+<p>Il costo del tuo biglietto é di: <strong>${finalCostTiket}&euro;</strong></p>
+`
