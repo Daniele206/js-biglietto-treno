d56ee6595ba192cc288290ac95c916a2b2e9a112
Daniele206 <danieledeieso2004@gmail.com>
2024-02-20 15:20:52 +0100

    [add] calc discount and [apply] to tiket-cost

--- JS/script.js
+++ JS/script.js
@@ -8,3 +8,17 @@ const costPerKm = 0.21;
 
 const fullCostTiket = chilometers * costPerKm;
 console.log(fullCostTiket);
+
+const discountTiketUnder18 = fullCostTiket * 20 / 100;
+console.log(discountTiketUnder18);
+
+const discountTiketOver65 = fullCostTiket * 40 / 100;
+console.log(discountTiketOver65);
+
+let finalCostTiket = '';
+
+if(age < 18) finalCostTiket = fullCostTiket - discountTiketUnder18;
+
+if(age > 65) finalCostTiket = fullCostTiket - discountTiketOver65;
+
+console.log(finalCostTiket);
